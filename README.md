5c11fa2a1ee3515e080792a96a1848f2bfc46c5e
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-11-04 10:22:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# QuPath ABBA extension
+
+This repo adds some support for the [ABBA plugin](https://biop.github.io/ijp-imagetoatlas/) in QuPath.
+
+## Installing
+
+You'll also need to install [`qupath-extension-warpy`](https://github.com/BIOP/qupath-extension-warpy) to access all functionalities of this extension.
+Download the latest `qupath-extension-abba-[version].jar` file from [releases](https://github.com/biop/qupath-extension-warpy/releases) and drag it onto the main QuPath window.
+
+If you haven't installed any extensions before, you'll be prompted to select a QuPath user directory.
+The extension will then be copied to a location inside that directory.
+
+You might then need to restart QuPath (but not your computer).
+
+## Citing
+
+TODO
+
+## Building
+
+You can build the QuPath ABBA extension from source with
+
+```bash
+gradlew clean build
+```
+
+The output will be under `build/libs`.
+
+* `clean` removes anything old
+* `build` builds the QuPath extension as a *.jar* file and adds it to `libs` 
---
+++ build.gradle.kts
@@ -0,0 +1,106 @@
+plugins {
+    `java-library`
+    // To create a shadow/fat jar, including dependencies
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    // To manage included native libraries
+    //  id 'org.bytedeco.gradle-javacpp-platform' version '1.5.6'
+    `maven-publish`
+}
+
+repositories {
+    // Use this only for local development!
+    //  mavenLocal()
+
+    mavenCentral()
+
+    maven("https://maven.scijava.org/content/repositories/releases")
+
+    maven("https://maven.scijava.org/content/repositories/snapshots")
+}
+
+ext.set("moduleName", "qupath.extension.abba")
+description = "QuPath extension to use Aligning Big Brain and Atlases"
+
+group = "ch.epfl.biop"
+// artifact = "qupath-extension-abba"
+version = "0.1.0-SNAPSHOT"
+
+dependencies {
+    val qupathVersion = "0.3.0" // For now
+
+    shadow("io.github.qupath:qupath-gui-fx:$qupathVersion")
+    shadow("org.slf4j:slf4j-api:1.7.30")
+    implementation("commons-io:commons-io:2.11.0")
+    implementation("net.imglib2:imglib2-realtransform:3.1.1")
+
+    testImplementation("io.github.qupath:qupath-gui-fx:$qupathVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+//processResources {
+//  from ("${projectDir}/LICENSE.txt") {
+//    into "licenses/"
+//  }
+//}
+
+tasks.register<Copy>("copyDependencies") {
+    description = "Copy dependencies into the build directory for use elsewhere"
+    group = "QuPath"
+
+    from(configurations.default)
+    into("build/libs")
+}
+
+/*
+ * Ensure Java 11 compatibility
+ */
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+    if ("sources" in project.properties)
+        withSourcesJar()
+    if ("javadocs" in project.properties)
+        withJavadocJar()
+}
+
+/*
+ * Create javadocs for all modules/packages in one place.
+ * Use -PstrictJavadoc=true to fail on error with doclint (which is rather strict).
+ */
+//def strictJavadoc = findProperty("strictJavadoc")
+//if (!strictJavadoc) {
+//    tasks.withType(Javadoc) {
+//        options.addStringOption("Xdoclint:none", "-quiet")
+//    }
+//}
+
+/*
+ * Avoid 'Entry .gitkeep is a duplicate but no duplicate handling strategy has been set.'
+ * when using withSourcesJar()
+ */
+//tasks.withType(org.gradle.jvm.tasks.Jar) {
+//    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+//}
+
+tasks.test { useJUnitPlatform() }
+
+// Skip fat jar publication
+components.java.withVariantsFromConfiguration(configurations.shadowRuntimeElements.get()) { skip() }
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components.java)
+    }
+    repositories {
+        maven {
+            name = "scijava"
+            //credentials(PasswordCredentials::class)
+            url = uri("https://maven.scijava.org/content/repositories/releases")
+        }
+    }
+}
+
+val SoftwareComponentContainer.java
+    get() = components.getByName("java") as AdhocComponentWithVariants
