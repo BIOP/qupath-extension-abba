5035beea957ec9a5b852b4752bc3161b701bef7c
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2022-01-14 11:17:32 +0100

    release version 0.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ description = "QuPath extension to use Aligning Big Brain and Atlases"
 
 group = "ch.epfl.biop"
 // artifact = "qupath-extension-abba"
-version = "0.1.2-SNAPSHOT"
+version = "0.1.2"
 
 dependencies {
     val qupathVersion = "0.3.0" // For now
