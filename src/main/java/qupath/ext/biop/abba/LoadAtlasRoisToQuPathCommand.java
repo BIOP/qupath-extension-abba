a31b5403380391c84e521ce030e9b10f39443dea
Carlo Castoldi <carlo.castoldi@outlook.com>
2025-03-26 16:28:32 +0100

    loadWarpedAtlasAnnotations() rewrite
    fix Waxholm's rat RoiSets not being imported
    bug introduced in efb5a275257bb990440987cf88fc438530eb7e75

--- src/main/java/qupath/ext/biop/abba/LoadAtlasRoisToQuPathCommand.java
+++ src/main/java/qupath/ext/biop/abba/LoadAtlasRoisToQuPathCommand.java
@@ -7,9 +7,10 @@ import qupath.ext.biop.abba.struct.AtlasOntology;
 import qupath.lib.gui.QuPathGUI;
 import qupath.fx.dialogs.Dialogs;
 import qupath.lib.images.ImageData;
+import qupath.lib.objects.PathObject;
 import qupath.lib.plugins.workflow.DefaultScriptableWorkflowStep;
 import qupath.lib.plugins.workflow.WorkflowStep;
-import qupath.lib.projects.Projects;
+import qupath.lib.scripting.QP;
 
 import java.awt.image.BufferedImage;
 import java.nio.file.Path;
@@ -51,27 +52,33 @@ public class LoadAtlasRoisToQuPathCommand implements Runnable {
         }
         if (doRun) {
             ImageData<BufferedImage> imageData = qupath.getImageData();
-            List<String> atlasNames = AtlasTools.getAvailableAtlasRegistration(imageData);
-            if (atlasNames.isEmpty()) {
-                Dialogs.showErrorMessage("No atlas registration found.", "You first need to export your registration from Fiji's ABBA plugin.");
-                logger.error("No atlas registration found."); // TODO : show an error message for the user
-                return;
-            }
-
-            String atlasName = atlasNames.get(0);
-            if (atlasNames.size()>1) {
-                logger.warn("Several atlases registration have been found. Importing atlas: "+atlasName);
-            }
-
-            Path ontologyPath = Paths.get(Projects.getBaseDirectory(qupath.getProject()).getAbsolutePath(), atlasName+"-Ontology.json");
-            AtlasOntology ontology = AtlasHelper.openOntologyFromJsonFile(ontologyPath.toString());
-            if (ontology == null) {
-                Dialogs.showErrorMessage("No atlas ontology found.",
-                        "Can't find the ontology corresponding to the atlas registration of the image."+
-                                "You first need to export your registration from Fiji's ABBA plugin.");
+            List<String> ontologyFiles = AtlasTools.getAvailableAtlasOntologyFiles();
+            if (ontologyFiles == null || ontologyFiles.isEmpty()) {
+                Dialogs.showErrorMessage("No atlas ontology found.", "You first need to export your registration from Fiji's ABBA plugin.");
                 logger.error("No atlas ontology found."); // TODO : show an error message for the user
                 return;
             }
+
+            // Get atlas ontology
+            String ontologyName;
+            String ontologyFile;
+            String ontologySuffix = "-Ontology.json";
+            if (ontologyFiles.size() > 1) {
+                ontologyName =
+                        Dialogs.showChoiceDialog("Atlas ontologies",
+                                "Please select the preferred atlas ontology used for registration.",
+                                ontologyFiles.stream()
+                                        .map(of -> of.substring(0, of.length() - ontologySuffix.length()))
+                                        .toList(),
+                                null);
+                ontologyFile = ontologyName+ontologySuffix;
+            } else {
+                ontologyFile = ontologyFiles.get(0);
+                ontologyName = ontologyFile.substring(0, ontologyFile.length() - ontologySuffix.length());
+            }
+
+            Path ontologyPath = Paths.get(QP.buildPathInProject(ontologyFile)).toAbsolutePath();
+            AtlasOntology ontology = AtlasHelper.openOntologyFromJsonFile(ontologyPath.toString());
 
             // Get naming possibilities
             String namingProperty =
@@ -83,10 +90,18 @@ public class LoadAtlasRoisToQuPathCommand implements Runnable {
             ontology.setNamingProperty(namingProperty);
 
             // Now we have all we need, the name whether to split left and right
-            AtlasTools.loadWarpedAtlasAnnotations(ontology, imageData, splitLeftRight, true);
+            PathObject rootAnnotation = AtlasTools.loadWarpedAtlasAnnotations(ontology, imageData, ontologyName, splitLeftRight, true);
+
+            if (rootAnnotation == null) {
+                Dialogs.showErrorMessage("No atlas registration found.",
+                        "Can't find the registration corresponding to the atlas ontology of the project."+
+                                "You first need to export your registration from Fiji's ABBA plugin.");
+                logger.error("No atlas registartion found."); // TODO : show an error message for the user
+                return;
+            }
 
             // Add a step to the workflow
-            String method = AtlasTools.class.getName()+".loadWarpedAtlasAnnotations(getCurrentImageData(), \""+namingProperty+"\", "+splitLeftRight+", true);";
+            String method = AtlasTools.class.getName()+".loadWarpedAtlasAnnotations(getCurrentImageData(), \""+ontologyName+"\", \""+namingProperty+"\", "+splitLeftRight+", true);";
             WorkflowStep newStep = new DefaultScriptableWorkflowStep("Load Brain RoiSets into Image", method);
             imageData.getHistoryWorkflow().addStep(newStep);
         }
